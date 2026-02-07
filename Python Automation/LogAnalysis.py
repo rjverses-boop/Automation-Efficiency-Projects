4a75142126acc23002cb816432561ef31250d129
rjverses-boop <rjverses@gmail.com>
2026-02-07 13:37:10 -0600

    Update LogAnalysis.py

--- Python Automation/LogAnalysis.py
+++ Python Automation/LogAnalysis.py
@@ -8,8 +8,8 @@ with open("system.log", "r") as f:
         if "Failed password" in line:
             user = re.search(r"for (invalid user )?(\w+)", line)
             if user:
-                failed_logins[user.group(2)] += 1
+                FailedLogins[user.group(2)] += 1
 
 print("Failed Login Attempts:")
-for user, count in failed_logins.items():
+for user, count in FailedLogins.items():
     print(f"{user}: {count}")
