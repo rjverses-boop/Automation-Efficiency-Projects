4384855baa4991d98fe9deacee69cda53064140f
rjverses-boop <rjverses@gmail.com>
2026-02-07 13:36:19 -0600

    Update SystemReports.ps1

--- PowerShell Automation/SystemReports.ps1
+++ PowerShell Automation/SystemReports.ps1
@@ -1,6 +1,6 @@
-Get-ADUser -Filter 'LockedOut -eq $true' | Export-Csv .\locked_out_users.csv -NoTypeInformation
-Get-ADUser -Filter 'Enabled -eq $false' | Export-Csv .\disabled_accounts.csv -NoTypeInformation
+Get-ADUser -Filter 'LockedOut -eq $true' | Export-Csv .\LockedOutUsers.csv -NoTypeInformation
+Get-ADUser -Filter 'Enabled -eq $false' | Export-Csv .\DisabledAccounts.csv -NoTypeInformation
 
 Get-WmiObject Win32_Product |
     Select Name, Version |
-    Export-Csv .\installed_software.csv -NoTypeInformation
+    Export-Csv .\InstalledSoftware.csv -NoTypeInformation
